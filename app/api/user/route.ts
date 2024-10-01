98d3da11bc79489f57a463fb25ce0893b7e09f38
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-02 03:12:06 +0530

    Removed unnecessary logs

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -15,7 +15,6 @@ export async function POST(req: NextRequest) {
             user
         })
     } catch (error) {
-        console.log(error)
         return NextResponse.json({
             message: "Failed to fetch user",
             success: false
@@ -41,7 +40,6 @@ export async function PUT(req: NextRequest) {
             user
         })
     } catch (error) {
-        console.log(error)
         return NextResponse.json({
             message: "Failed to update user",
             success: false
