f741087f4240be3a6298250b8bc4d49af70056c0
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-03 06:39:14 +0530

    [FEAT] Added routing to explore events page and option for signout

--- components/layout/Appbar.tsx
+++ components/layout/Appbar.tsx
@@ -28,16 +28,14 @@ const Appbar = async() => {
             <Link href={"/"}><IoTicketOutline className="text-3xl text-zinc-200 hover:text-zinc-300 duration-200 cursor-pointer" /></Link>
           </div>
           <div className="flex items-center gap-8">
-          <p className="hover:text-zinc-300 duration-200 font-semibold tracking-wide text-zinc-200 cursor-pointer">Explore</p>
+          <Link href='/explore'><div className="hover:text-zinc-300 duration-200 font-semibold tracking-wide text-zinc-200 cursor-pointer">Explore</div></Link>
           {session ? <DropdownMenu>
           <DropdownMenuTrigger className="bg-white text-black p-2 font-semibold rounded-full focus:outline-none">{session.user?.name?.split(" ").map((name) => name[0]).join("")}</DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem><Link href={`/profile/${userId}`}>Profile</Link></DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuItem><Link href="/api/auth/signout">Sign out</Link></DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         : <Link href={"/api/auth/signin"}><button className="bg-zinc-200 rounded-full px-2 py-1 hover:bg-zinc-500 hover:text-white text-zinc-500 font-semibold duration-200">Sign in</button></Link>}
