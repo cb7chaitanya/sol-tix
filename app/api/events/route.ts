05da9479beaaa0c7b1ee13f43c112cc51dba41cf
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-03 06:39:50 +0530

    [FEAT] Integrated S3 for image storage

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,5 +1,15 @@
 import prisma from "@/app/db";
 import { NextRequest, NextResponse } from "next/server";
+import { PutObjectCommand, GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials:{
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+    }
+})
 
 export async function GET() {
     try{
@@ -20,11 +30,27 @@ export async function GET() {
 export async function POST(req: NextRequest) {
     const { name, description, image, capacity, startDate, endDate, isPaid, UTCOFFset, isVirtual, ticketPrice, location, requireApproval, publicStatus, zoomAddress } = await req.json()
     try {
+
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME || '',
+            Key: name,
+            Body: image,
+        }
+        const command = new PutObjectCommand(params);
+        await s3Client.send(command);
+        
+        const getObjectParams = {
+            Bucket: process.env.AWS_BUCKET_NAME || '',
+            Key: name
+        }
+        const signedUrlCommand = new GetObjectCommand(getObjectParams);
+        const signedUrl = await getSignedUrl(s3Client, signedUrlCommand, { expiresIn: 0 });
+
         const event = await prisma.event.create({
             data: {
                 name: name,
                 description:description,
-                image: image,
+                image: signedUrl,
                 capacity: capacity,
                 startDate: startDate,
                 endDate: endDate,
